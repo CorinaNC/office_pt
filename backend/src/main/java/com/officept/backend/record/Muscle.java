fc149f245f7840a96d0c7ed061e326df783b126b
Chau T. <chaunmt.102@gmail.com>
2025-11-22 17:47:46 -0600

    more muscles

--- backend/src/main/java/com/officept/backend/record/Muscle.java
+++ backend/src/main/java/com/officept/backend/record/Muscle.java
@@ -4,17 +4,22 @@ import lombok.Getter;
 
 @Getter
 public enum Muscle {
-    INTEROSSEI(0, "Interossei"),
-    PALMARIS_LONGUS(1, "Palmaris longus")
-    ;
-    // TODO: Add more please
+    FLEXOR_CARPI_RADIALIS(0, "Flexor carpi radialis"),
+    FLEXOR_CARPI_ULNARIS(1, "Flexor carpi ulnaris"),
+    EXTENSOR_CARPI_RADIALIS_BREVIS(2, "Extensor carpi radialis brevis"),
+    EXTENSOR_CARPI_RADIALIS_LONGUS(3, "Extensor carpi radialis longus"),
+    EXTENSOR_CARPI_ULNARIS(4, "Extensor carpi ulnaris"),
+    EXTENSOR_POLLICIS_LONGUS(5, "Extensor pollicis longus"),
+    ABDUCTOR_POLLICIS_LONGUS(6, "Abductor pollicis longus"),
+    EXTENSOR_POLLICIS_BREVIS(7, "Extensor pollicis brevis"),
+    CARPAL_TUNNEL(8, "Carpal tunnel");
 
     private final int value;
-    private final String description;
+    private final String label;
 
-    Muscle(int value, String description) {
+    Muscle(int value, String label) {
         this.value = value;
-        this.description = description;
+        this.label = label;
     }
 
     public static Muscle fromValue(int value) throws IllegalArgumentException {

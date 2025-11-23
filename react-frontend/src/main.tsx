a6ab20540dc8306cdbe719b64067d22ab63b33eb
lindseyoh <ohlindsey1@gmail.com>
2025-11-22 23:56:52 -0600

    AI chat tab added

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -9,6 +9,7 @@ import Home from "./pages/Home.tsx";
 import Videos from "./pages/Videos.tsx";
 import Camera from "./pages/Camera.tsx";
 import Stretch from "./pages/Stretch.tsx";
+import Test from "./pages/Test.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -21,6 +22,7 @@ createRoot(document.getElementById("root")!).render(
             <Route path="/videos" element={<Videos />} />
             <Route path="/camera" element={<Camera />} />
             <Route path="/stretch" element={<Stretch />} />
+            <Route path="/test" element={<Test />} />
           </Route>
         </Routes>
       </BrowserRouter>

e92c2ee560c9f208f5369e7fed997e113675cfd4
CorinaNC <corina0615@gmail.com>
2025-11-23 04:26:09 -0600

    removed random routes that aren't needed

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -7,8 +7,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./components/theme.ts";
 import Home from "./pages/Home.tsx";
 import Videos from "./pages/Videos.tsx";
-import Stretch from "./pages/Stretch.tsx";
-import Test from "./pages/Test.tsx";
 import Chat from "./pages/Chat.tsx";
 import LiveWebFeed from "./pages/LiveWebFeed.js";
 import Survey from "./pages/Survey.tsx";
@@ -26,8 +24,6 @@ createRoot(document.getElementById("root")!).render(
             <Route path="/" element={<BackButtonWrapper />}>
               <Route path="/videos" element={<Videos />} />
               <Route path="/camera" element={<LiveWebFeed />} />
-              <Route path="/stretch" element={<Stretch />} />
-              <Route path="/test" element={<Test />} />
               <Route path="/chat" element={<Chat />} />
               <Route path="/survey" element={<Survey />} />
               <Route path="/diagram" element={<Diagram />} />
--- react-frontend/src/pages/Stretch.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Stretch = () => {
-  return (
-    <div>Stretch</div>
-  )
-}
-
-export default Stretch

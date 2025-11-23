fe494dd734c459dac2ecdc88c4353b19c5622437
Chau T. <chaunmt.102@gmail.com>
2025-11-22 23:12:03 -0600

    cleaning

--- backend/src/test/java/com/officept/backend/FlaskAPITest.java
+++ backend/src/test/java/com/officept/backend/FlaskAPITest.java
@@ -3,18 +3,21 @@ package com.officept.backend;
 import com.officept.backend.dto.ChatInputDTO;
 import com.officept.backend.dto.ChatOutputDTO;
 import com.officept.backend.service.ChatService;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class FlaskAPITest {
     Logger logger = LoggerFactory.getLogger(FlaskAPITest.class);
 
@@ -22,6 +25,7 @@ public class FlaskAPITest {
     private ChatService chatService;
 
     @Test
+    @Ignore  // Only run when python server is on
     public void testManualChat() {
         ChatInputDTO input = ChatInputDTO.builder()
                 .withConversationId(null)
